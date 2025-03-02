e90a7c2cad9497abd0d47cbd62a5ff4f2dfcb524
Ojas <149180655+ojasarora77@users.noreply.github.com>
2025-03-02 17:49:39 +0000

    minor

--- app/certificate/[id]/page.tsx
+++ app/certificate/[id]/page.tsx
@@ -1,9 +1,11 @@
 // app/certificate/[id]/page.tsx
 'use client';
 
-import DemoCertificateView from './page';
+import { useParams } from 'next/navigation';
+import DemoCertificateView from '/Users/ojasarora/SouliFicate/app/pitch/page';
 
 export default function CertificateView() {
+  const params = useParams();
   
   // For demo purposes, always show the hardcoded demo certificate
   return <DemoCertificateView />;
